960841a1b6fcfe42634558e778bf201f935a3e81
Orenoid <2367058391@qq.com>
2020-03-10 16:59:57 +0800

    fix log format

--- app/utils/hooks.py
+++ app/utils/hooks.py
@@ -5,9 +5,9 @@ def log_request_params():
     current_app.logger.debug(f'request url: {request.url_root}')
     current_app.logger.debug(f'request data: {request.data}')
     current_app.logger.debug(f'request values: {request.values}')
-    current_app.logger.debug(f'request headers: {request.headers}')
+    current_app.logger.debug(f'request headers: ' + repr(request.headers).replace("\n", " "))
 
 
 def log_response(response: Response):
-    current_app.logger.debug(f'response: {response.data}')
+    current_app.logger.debug(f'response: {response.data}\n')
     return response
