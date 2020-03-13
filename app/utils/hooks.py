e20e2dda338b010baf0e2f161fe7c39ea632abab
Orenoid <2367058391@qq.com>
2020-03-13 14:07:23 +0800

    fix proxy ip

--- app/utils/hooks.py
+++ app/utils/hooks.py
@@ -2,7 +2,8 @@ from flask import request, current_app, Response
 
 
 def log_request_params():
-    current_app.logger.debug(f'request url: {request.url}')
+    client_ip = get_client_ip()
+    current_app.logger.debug(f'request {request.method} url: {request.url} from: {client_ip}')
     current_app.logger.debug(f'request data: {request.data}')
     current_app.logger.debug(f'request values: {request.values}')
     current_app.logger.debug(f'request headers: ' + repr(request.headers).replace("\n", " "))
@@ -11,3 +12,12 @@ def log_request_params():
 def log_response(response: Response):
     current_app.logger.debug(f'response: {response.data}\n')
     return response
+
+
+def get_client_ip():
+    if request.headers.get('X-Forwarded-For'):
+        return request.headers['X-Forwarded-For']
+    elif request.headers.get('X-Real-IP'):
+        return request.headers.get('X-Real-IP')
+    else:
+        return request.remote_addr
