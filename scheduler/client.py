a00b235d6c191c693cfd9289c04297e20922290d
Orenoid <2367058391@qq.com>
2020-03-17 18:50:52 +0800

    update example

--- scheduler/client.py
+++ scheduler/client.py
@@ -6,7 +6,7 @@ conn = rpyc.connect('localhost', int(os.environ.get('APS_SERVER_PORT')))
 """
 # example:
 
-job = conn.root.add_job('server:create_new_bill', 'interval',
+job = conn.root.add_job('scheduler.server:create_new_bill', 'interval',
                         args=[user_id, '1.00', 'category', 'out'], seconds=10, id='job_id')
 conn.root.remove_job('job_id')
 """
